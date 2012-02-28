cd8cea068bcf0a82a1767db8e19fc1041aaf9e2d
Jan Pokorsky <pokorsky@gmail.com>
2012-02-28 16:12:50 +0100

    repackaged

--- src/main/java/cz/incad/imgsupport/ScaleTiff.java
+++ src/main/java/cz/incad/imgsupport/ScaleTiff.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package cz.incad.tiffsup;
+package cz.incad.imgsupport;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
